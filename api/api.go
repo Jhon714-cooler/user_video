2d663a31de2e7555cf2a4c3a8dca09ff3c88f4af
tesla <2605791375@qq.com>
2023-07-28 12:49:23 +0800

    处理剪辑vieo请求

--- api/api.go
+++ api/api.go
@@ -3,10 +3,25 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
+type Req_clipVideo struct {
+	VideoURL  string `json:"video_url" binding:"required"`
+	StartTime string `json:"start_time" binding:"required"`
+	EndTime   string `json:"end_time" binding:"required"`
+}
+
 func UploadVideo(c *gin.Context) {
-	log.Println("hello")
+	var req Req_clipVideo
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error:": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message":       "cliping video",
+		"cliResult_url": "查询剪辑进度",
+		"video_id":      "视频id 查询剪辑进度",
+	})
 }
 func GetVideo(c *gin.Context) {
 	log.Println("hello")
