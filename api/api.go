336feec8cfdab601b1fb11094e63a588d026c94d
tesla <2605791375@qq.com>
2023-07-28 14:53:52 +0800

    修改剪辑请求结构体，添加Userid

--- api/api.go
+++ api/api.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Req_clipVideo struct {
+	UserId    uint   `json:"user_id" binding:"required"`
 	VideoURL  string `json:"video_url" binding:"required"`
 	StartTime string `json:"start_time" binding:"required"`
 	EndTime   string `json:"end_time" binding:"required"`
